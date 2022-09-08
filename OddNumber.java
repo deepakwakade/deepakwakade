67851f8b394a734aa7cb87e37386eb861f478fc0
deepakwakade <113004425+deepakwakade@users.noreply.github.com>
2022-09-08 13:15:48 +0530

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,16 @@
+package dpack;
+
+public class OddNumber
+ {
+  public static void main(String[] args)
+  {
+	    //for printing the odd numbers from 1 20
+		for(int i=1;10;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+	 }
+ }
---
+++ Vowels2.java
@@ -0,0 +1,25 @@
+class Vowels2
+{
+  public static void main(String [] args)
+{
+  char a='e';
+
+  if (a=='e')
+   System.out.println("This is a Vowels"+a);
+
+
+   else if (a=='i')
+   System.out.println("This is a Vowels"+a);
+
+   else if (a=='o')
+   System.out.println("This is a Vowels"+a);
+
+   else if (a=='u')
+   System.out.println("This is a Vowels"+a);
+   
+  else
+    System.out.println("This is a Consonounts"+a);
+   
+
+}
+}
