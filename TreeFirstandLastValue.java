8f13aae553d42824b2717a207e7c6b6ed2e3c68a
deepakwakade <113004425+deepakwakade@users.noreply.github.com>
2022-11-03 13:27:34 +0530

    Labtest 9 Q . 2

---
+++ TreeFirstandLastValue.java
@@ -0,0 +1,25 @@
+package Collection.com;
+import java.util.TreeMap;
+
+public class TreeFirstandLastValue {
+   public static void main(String[] args) {
+      System.out.println("(StudentList.java)!\n");
+      TreeMap <Integer, String>tMap = new TreeMap<Integer, String>();
+      tMap.put(1, "Sushil");
+      tMap.put(2, "Robet");
+      tMap.put(3, "Doney");
+      tMap.put(4, "Junior");
+      tMap.put(5, "Peter");
+      tMap.put(6, "Parker");
+      tMap.put(7, "Chris");
+      System.out.println("Keys of tree map: " + tMap.keySet());
+      System.out.println("Values of tree map: " + tMap.values());
+      System.out.println(" ");
+
+      System.out.println(
+         "First element of ArryList Student: " + tMap.firstKey() + " Value: " + tMap.get(tMap.firstKey()));
+      System.out.println(
+         "Last element of ArryList Student:  " + tMap.lastKey() + " Value: "+ tMap.get(tMap.lastKey()));
+
+   }
+}
